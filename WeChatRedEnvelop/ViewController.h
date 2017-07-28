677e1432d7aa9becc938551cedbffd8f62cb17ec
lifp <lifp@mmwipo.com>
2017-07-28 20:57:30 +0800

    Initial Commit

---
+++ WeChatRedEnvelop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChatRedEnvelop
+//
+//  Created by N年後 on 2017/7/28.
+//  Copyright © 2017年 lifp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeChatRedEnvelop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeChatRedEnvelop
+//
+//  Created by N年後 on 2017/7/28.
+//  Copyright © 2017年 lifp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
